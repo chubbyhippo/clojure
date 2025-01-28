7c02f1b26da15ad1273d4998d0431c1c5f8aa7c2
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-28 13:44:48 +0700

    Create install-clojure.sh

---
+++ install-clojure.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+curl -L -O https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh
+chmod +x linux-install.sh
+sudo ./linux-install.sh
+rm linux-install.sh
